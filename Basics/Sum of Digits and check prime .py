11c3872c55839f8f36773b79b6a96cb9d51ae05d
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-12 19:40:09 +0530

    Update Sum of Digits and check prime .py

--- Basics/Sum of Digits and check prime .py
+++ Basics/Sum of Digits and check prime .py
@@ -1,15 +1,15 @@
-def prime (num):
-    count = 0
-    for i in range(2,num):
+def prime(num):
+    count = 0 
+    for i in range(2,n):
         if (num % i == 0):
             count += 1 
-    if count > 0:
-        print(num," not prime")
+    if count == 0 :
+        print("Prime")
     else:
-        print(num," prime")
-            
-n = input()
-total = 0 
-for i in n :
-    total += int(i)
-prime(total)
+        print("NOt Prime")
+n = int(input())
+sum = 0 
+for i in range(0,n+1):
+    sum += i
+print(sum)
+prime(sum)
