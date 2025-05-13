347b3d02b454e08351fbbacac1d1389ac7cd03a6
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 12:57:06 +0530

    Update Search Key in Dictionary .py

--- Dictonaries/Search Key in Dictionary .py
+++ Dictonaries/Search Key in Dictionary .py
@@ -12,29 +12,9 @@ True
 '''
 
 # CODE  : 
-def user(entries):
-    data = {}
-    for i in range(entries):
-        key = input("Enter Key : ")
-        value = int(input("Enter Value : "))
-        data[key] = value 
-    return data
-
-def search_key(dictionary , key):
-    if key in dictionary :
-        return True
-        
-    else:
-        return False
-        
-    
-def main():
-    entries = int(input("Enter Entries : "))
-    result = user(entries)
-    print(result)
-    key = input("Enter key you want to check : ")
-    res = search_key(result,key)
-    print(res)
-    
-    
-main()
+dict = eval(input())
+key = input()
+if key in dict : 
+    print ("Found")
+else :
+    print("Not Found")
