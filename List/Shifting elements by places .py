b67ba056a333eb116d54fc9c818867266a984f3b
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 09:06:49 +0530

    Update Shifting elements by places .py

--- List/Shifting elements by places .py
+++ List/Shifting elements by places .py
@@ -1,12 +1,25 @@
 lst = eval(input())
-k = int(input())
+n = int(input("Enter places of shift : "))
+result = []*len(lst)
 
-l = len(lst)
-k = k % l 
+if n > 0 :       #right shifting
+    result = lst[-n:] + lst[:-n]
+    print(result)
+if n < 0 :       #left shifitng 
+    result = lst[-n:] + lst[:-n]
+    print(result)
 
-if k > 0 :
-    res = lst[-k:] + lst[:-k]
-    print(res)
-else:
-    print(res)
-    
+'''
+# TERMINAL 
+
+[1,2,3,4,5,6,7,8,9]
+Enter places of shift : 2   (right shift)
+[8, 9, 1, 2, 3, 4, 5, 6, 7]
+
+
+[1,2,3,4,5,6,7,8,9]
+Enter places of shift : -2   (left shift)
+[3, 4, 5, 6, 7, 8, 9, 1, 2]
+
+
+''''
