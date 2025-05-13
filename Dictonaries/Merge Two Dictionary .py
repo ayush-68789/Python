f6c3393e1e114d46a4f41e428138d29d8ca4528a
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 11:45:58 +0530

    Update Merge Two Dictionary .py

--- Dictonaries/Merge Two Dictionary .py
+++ Dictonaries/Merge Two Dictionary .py
@@ -1,37 +1,14 @@
 '''
 TERMINAL : 
-Input Dictionary 1 --- 
-Enter Entry : 2
-Enter Key : A
-Enter Value : 1
-Enter Key : B
-Enter Value : 2
-Input Dictionary 2 --- 
-Enter Entry : 3
-Enter Key : C
-Enter Value : 4
-Enter Key : D
-Enter Value : 6
-Enter Key : E
-Enter Value : 7
-Merged Dictionary is : 
- {'A': 1, 'B': 2, 'C': 4, 'D': 6, 'E': 7}
+{1 : 'a' , 2:'b'}
+{2:'c' , 3 : 'd'}
+{1: 'a', 2: 'c', 3: 'd'}
+
 
  '''
-def user() :
-    entry = int(input("Enter Entry : "))
-    data = {}
-    for i in range(entry):
-        key = input("Enter Key : ")
-        value = int(input("Enter Value : "))
-        data[key] = value 
-    return data
-    
-    
-print("Input Dictionary 1 --- ")
-Dict1 = user()
-print("Input Dictionary 2 --- ")
-Dict2 = user()
-merge = Dict1.copy()
-merge.update(Dict2)
-print("Merged Dictionary is : \n",merge)
+dict_1 = eval(input())
+dict_2 = eval(input())
+merge = {}
+merge = dict_1.copy()
+merge.update(dict_2)
+print(merge)
