b5b7a678a8479b80d8f64d9b05839a0ec884bbcb
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 13:36:32 +0530

    Update Update Dictionary(Add key value pair ) .py

--- Dictonaries/Update Dictionary(Add key value pair ) .py
+++ Dictonaries/Update Dictionary(Add key value pair ) .py
@@ -1,40 +1,18 @@
-def dict_input(entries):
-    data = {}
-    for i in range(entries):
-        key = input("Enter key : ")
-        value = int(input("Enter Value : "))
-        data[key] = value
-    return data
-    
-def update(data_input):
-    new_key = input("Enter New key : ")
-    new_value = int(input("Enter New Value : "))
-    data_input[new_key] = new_value
-    return data_input
-    
-    
-entries = int(input("Enter number of entries : "))
-data_input = dict_input(entries)
-print("Resulting Dictionary : ")
-print(data_input)
- 
-updated = update(data_input)
-print("Updated Dictionary : ")
-print(updated)
+dict = eval(input())
+new_dict = {}
+key = input()
+value =int(input())
+new_dict[key] = value 
+
+dict.update(new_dict)
+print(dict)
 
 
 '''
 Terminal : 
-Enter number of entries : 2
-Enter key : AT
-Enter Value : 32
-Enter key : AU
-Enter Value : 30
-Resulting Dictionary : 
-{'AT': 32, 'AU': 30}
-Enter New key : AS
-Enter New Value : 38
-Updated Dictionary : 
-{'AT': 32, 'AU': 30, 'AS': 38}
+{'a' : 2 , 'b' : 3 }
+c
+5
+{'a': 2, 'b': 3, 'c': 5}
 
 '''
