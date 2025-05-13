8abd5210bcebd329ecd064c152cd330da585b008
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 10:54:42 +0530

    Update Count Frequency of char  .py

--- String/Count Frequency of char  .py
+++ String/Count Frequency of char  .py
@@ -6,3 +6,18 @@ for char in set(str) :
         if c == char :
             count += 1
     print(f"'{char}':{count}",end=" ")
+
+
+
+# OR USING DICTONARY 
+
+str = input()
+str = str.replace(" ","")
+freq = {}
+for char in str :
+    if char in freq :
+        freq[char] += 1 
+        
+    else :
+        freq[char] = 1 
+print(freq)
