23c401e9c6a9c4966e733b8c1def8f0f2fa67597
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-13 13:26:25 +0530

    Update Total product of Values in DIctionary .py

--- Dictonaries/Total product of Values in DIctionary .py
+++ Dictonaries/Total product of Values in DIctionary .py
@@ -1,30 +1,14 @@
 '''
 TERMINAL :
 Input Dictionary --- 
-Enter Entry : 3
-Enter Key : a
-Enter Value : 12
-Enter Key : b
-Enter Value : 3
-Enter Key : c
-Enter Value : 4
-Total Product :  144
+{'a' : 3 , 'b' : 2 }
+6
+
 '''
 
 
-def user() :
-    entry = int(input("Enter Entry : "))
-    data = {}
-    for i in range(entry):
-        key = input("Enter Key : ")
-        value = int(input("Enter Value : "))
-        data[key] = value 
-    return data
-    
-    
-print("Input Dictionary --- ")
-Dict = user()
-multi = 1
-for i in Dict.values():
-    multi *= i
-print("Total Product : ",multi)
+dict = eval(input())
+prod = 1 
+for i in dict.values() :
+    prod *= i 
+print(prod)
